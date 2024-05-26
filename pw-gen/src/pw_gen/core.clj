3c22729d0c68f27272545c5585e0db6a0a6e3dd4
Mike-AMD <mikew670@gmail.com>
2024-05-26 16:16:51 -0500

    added something suitable for 16 char passwords

--- pw-gen/src/pw_gen/core.clj
+++ pw-gen/src/pw_gen/core.clj
@@ -2,40 +2,48 @@
     (:gen-class)
     (:require [clojure.string :as str]))
 
-(defn isSuitableWord [word]
-      (let [wordString (str word)
-            wordCount (count wordString)
-            distinctCount (count (distinct (str/split wordString #"")))]
-           (and
-             (> (count wordString) 4)
-             (< (count wordString) 6)
-             (< (- wordCount distinctCount) 2))))
+(defn isSuitableWord [num word]
+  (let [wordString    (str word)
+        wordCount     (count wordString)
+        distinctCount (count (distinct (str/split wordString #"")))]
+    (and
+     (> (count wordString) (- num 1))
+     (< (count wordString) (+ num 1))
+     (< (- wordCount distinctCount) 2))))
 
 
 (defn injectRandomSpecial [specialChars pw]
-      (let [pwString (str pw)
-            [begin end] (split-at
-                          (rand-int (count pwString))
-                          (str/split pwString #""))
-            rndChar (rand-nth (str/split specialChars #""))]
-           (concat begin rndChar end)))
+  (let [pwString    (str pw)
+        [begin end] (split-at
+                     (rand-int (count pwString))
+                     (str/split pwString #""))
+        rndChar     (rand-nth (str/split specialChars #""))]
+    (concat begin rndChar end)))
 
-(defn getSuitableWords [path]
-      (filter #(isSuitableWord %)
-              (map #(str/trim %)
-                (str/split (slurp path) #"\n"))))
+(defn getSuitableWords [num path]
+  (filter #(isSuitableWord num %)
+          (map #(str/trim %)
+               (str/split (slurp path) #"\n"))))
 
 (defn -main [& args]
 
-      (let [wordsFile "./resources/words_alpha.txt"
-            specialChars "!@#$%^&*()_?><{}[]:"
-            wordSeparator "-"
-            suitableWords (getSuitableWords wordsFile)]
+  (let [wordsFile     "./resources/words_alpha.txt"
+        commonWordsFile "./resources/3k_common.txt"
+        specialChars  "!@#$%^&*()_?><{}[]:"
+        wordSeparator "-"
+        fiveLetterWords (getSuitableWords 5 wordsFile)]
 
-           (def passWithoutChar
-              (str/join
-               wordSeparator
-               (take 3 (shuffle suitableWords))))
+    (def passWithoutChar
+      (str/join
+       wordSeparator
+       (take 3 (shuffle fiveLetterWords))))
 
-           (println (reduce #(str %1 %2) (injectRandomSpecial specialChars passWithoutChar)))))
+    (println
+      (str/join
+       ""
+       (take 4 (shuffle (getSuitableWords 4 commonWordsFile)))))
+
+
+    (println
+      (reduce #(str %1 %2) (injectRandomSpecial specialChars passWithoutChar)))))
 
